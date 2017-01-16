e5a324cb5bee2d29852177fa27151719a34c39cc
val kharitonashvili <val kharitonashvili>
2017-01-16 11:00:38 -0800

    Pro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 
+export class Navigaton {
+  id : number;
+  menu: string;
+}
+
+const NAVIGATION: Navigaton[] = [
+  { id: 11, menu: 'ვალერი' },
+  { id: 12, menu: 'კოტე' },
+  { id: 13, menu: 'გიგა' },
+  { id: 14, menu: 'ლეონი' }
+];
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: 'navigation',
+  templateUrl: '../templates/navigation.html'
 })
 export class AppComponent {
   title = 'app works! bitches';
+
+  name = 'გიგა როგორ ხარ ?';
+
+  nav = NAVIGATION;
 }
